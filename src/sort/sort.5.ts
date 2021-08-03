72e5f2e184594b18569099f668568d6e7500569c
Fay <xufayflying@gmail.com>
2021-08-03 18:16:18 +0800

    Update: quick sort

---
+++ src/sort/sort.5.ts
@@ -0,0 +1,85 @@
+function swap(array: number[], i: number, j: number) {
+  const temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
+}
+
+/**
+ * 以第一个元素为基准，小于基准的和 left 交换位置，之后交转 left 和 pivot 的位置
+ * @param array
+ * @param left 左指针，慢指针
+ * @param right 右指针，快指针
+ */
+function partition_1(array: number[], left: number, right: number) {
+  const pivotIndex = left
+
+  for (let i = left + 1; i <= right; i++) {
+    if (array[i] < array[pivotIndex]) {
+      left++
+      swap(array, i, left)
+    }
+  }
+  swap(array, pivotIndex, left)
+  return left
+}
+
+/**
+ * 以最后一个元素为基准，小于基准的和 left 交换位置，之后交转 left 和 pivot 的位置
+ * @param array
+ * @param left 左指针，慢指针
+ * @param right 右指针，快指针
+ */
+function partition_2(array: number[], left: number, right: number) {
+  const pivotIndex = right
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < array[pivotIndex]) {
+      swap(array, i, left)
+      left++
+    }
+  }
+  swap(array, pivotIndex, left)
+  return left
+}
+
+/**
+ * 以第一个元素为基准，动态调整左右指针
+ * @param array
+ * @param left 左指针
+ * @param right 右指针
+ * @returns
+ */
+function partition_3(array: number[], left: number, right: number) {
+  const pivot = array[left]
+  while (left < right) {
+    while (left < right && array[right] > pivot) {
+      right--
+    }
+    array[left] = array[right]
+    while (left < right && array[left] <= pivot) {
+      left++
+    }
+    array[right] = array[left]
+  }
+  array[left] = pivot
+  return left
+}
+
+function quickSort(array: number[], left: number, right: number) {
+  if (left < right) {
+    const partitionIndex = partition_2(array, left, right)
+    quickSort(array, left, partitionIndex - 1)
+    quickSort(array, partitionIndex + 1, right)
+  }
+}
+
+/**
+ * 快速排序
+ */
+function sort(list: number[]) {
+  const l = list.length
+  if (l <= 1) return
+  quickSort(list, 0, list.length - 1)
+}
+
+export default sort
--- src/sort/sort.test.ts
+++ src/sort/sort.test.ts
@@ -3,8 +3,9 @@ import sort_1 from './sort.1'
 import sort_2 from './sort.2'
 import sort_3 from './sort.3'
 import sort_4 from './sort.4'
+import sort_5 from './sort.5'
 
-const Functions = [sort_0, sort_1, sort_2, sort_3, sort_4]
+const Functions = [sort_0, sort_1, sort_2, sort_3, sort_4, sort_5]
 
 Functions.forEach((sort, index) => {
   describe(`[排序](sort) - ${index}`, () => {
@@ -12,6 +13,18 @@ Functions.forEach((sort, index) => {
       const list = [5, 9, 3, 1, 2, 8, 4, 7, 6]
       sort(list)
       expect(list).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+
+    test(`[5, 9, 3, 1, 2, 3]`, () => {
+      const list = [5, 9, 3, 1, 2, 3]
+      sort(list)
+      expect(list).toEqual([1, 2, 3, 3, 5, 9])
+    })
+
+    test(`[5, 2, 9, 3, 1, 3, 2, 3]`, () => {
+      const list = [5, 2, 9, 3, 1, 3, 2, 3]
+      sort(list)
+      expect(list).toEqual([1, 2, 2, 3, 3, 3, 5, 9])
     })
 
     test(`[]`, () => {
