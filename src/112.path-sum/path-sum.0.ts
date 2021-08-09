a3f8f6034260422cea7f2827af84e2e205c8b537
Fay <xufayflying@gmail.com>
2021-08-09 21:04:32 +0800

    Add: 112. 路径总和

---
+++ src/112.path-sum/path-sum.0.ts
@@ -0,0 +1,15 @@
+import TreeNode from '../utils/TreeNode'
+
+function hasSum(node: TreeNode | null, targetSum: number): boolean {
+  if (node === null) return false
+  if (node.left === null && node.right === null) {
+    return node.val === targetSum
+  }
+  return hasSum(node.left, targetSum - node.val) || hasSum(node.right, targetSum - node.val)
+}
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  return hasSum(root, targetSum)
+}
+
+export default hasPathSum
---
+++ src/112.path-sum/path-sum.test.ts
@@ -0,0 +1,28 @@
+import TreeNode from '../utils/TreeNode'
+import hasPathSum_0 from './path-sum.0'
+
+const { fromArray } = TreeNode
+
+const Collections = [hasPathSum_0]
+
+Collections.forEach((hasPathSum, index) => {
+  describe(`[112. 路径总和](path-sum) - ${index}`, () => {
+    test(`[5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1], 22 => true`, () => {
+      const node = fromArray([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1])
+      const received = hasPathSum(node, 22)
+      expect(received).toBe(true)
+    })
+
+    test(`[1, 2, 3], 5 => false`, () => {
+      const node = fromArray([1, 2, 3])
+      const received = hasPathSum(node, 22)
+      expect(received).toBe(false)
+    })
+
+    test(`[1, 2], 0 => false`, () => {
+      const node = fromArray([1, 2])
+      const received = hasPathSum(node, 22)
+      expect(received).toBe(false)
+    })
+  })
+})
