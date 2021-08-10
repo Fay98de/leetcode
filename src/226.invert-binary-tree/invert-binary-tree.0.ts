bc51bb280456434c91ecf6763ce74797a8dce5f4
Fay <xufayflying@gmail.com>
2021-08-10 14:27:26 +0800

    Add: 226. 翻转二叉树

---
+++ src/226.invert-binary-tree/invert-binary-tree.0.ts
@@ -0,0 +1,17 @@
+import TreeNode from '../utils/TreeNode'
+
+function invert(node: TreeNode | null) {
+  if (node === null) return
+  const temp = node.left
+  node.left = node.right
+  node.right = temp
+  invert(node.left)
+  invert(node.right)
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  invert(root)
+  return root
+}
+
+export default invertTree
---
+++ src/226.invert-binary-tree/invert-binary-tree.test.ts
@@ -0,0 +1,16 @@
+import TreeNode from '../utils/TreeNode'
+import invertTree_0 from './invert-binary-tree.0'
+
+const { fromArray, toArray } = TreeNode
+
+const Collections = [invertTree_0]
+
+Collections.forEach((invertTree, index) => {
+  describe(`[226. 翻转二叉树](invert-binary-tree) - ${index}`, () => {
+    test(`[4, 2, 7, 1, 3, 6, 9] => [4, 7, 2, 9, 6, 3, 1]`, () => {
+      const node = fromArray([4, 2, 7, 1, 3, 6, 9])
+      const received = toArray(invertTree(node))
+      expect(received).toEqual([4, 7, 2, 9, 6, 3, 1])
+    })
+  })
+})
